ba5a53fd1baa4e3cd99564dd96a507a99094c6ac
imath <math.viet@gmail.com>
2020-05-03 18:44:35 +0200

    Add settings to let site owners set their preference

--- inc/admin.php
+++ inc/admin.php
@@ -82,3 +82,122 @@ function reception_admin_disallow_blocks( $allowed_block_types, $post ) {
 	return $allowed_block_types;
 }
 add_filter( 'allowed_block_types', 'reception_admin_disallow_blocks', 10, 2 );
+
+/**
+ * Sanitizes the settings about the Block based member front.
+ *
+ * @since 1.0.0
+ *
+ * @param boolean|string $value The value to sanitize.
+ * @return boolean The sanitized value.
+ */
+function reception_admin_sanitize_block_based_member_front( $value ) {
+	if ( isset( $_POST['option_page'] ) && 'buddypress' === $_POST['option_page'] ) { // phpcs:ignore
+		$value = true;
+
+		if ( isset( $_POST['reception_disable_block_based_member_front'] ) && 1 === (int) $_POST['reception_disable_block_based_member_front'] ) { // phpcs:ignore
+			$value = false;
+		}
+	}
+
+	return rest_sanitize_boolean( $value );
+}
+
+/**
+ * Outputs the form elements to set the site owner preference about the Block based member front.
+ *
+ * @since 1.0.0
+ */
+function reception_admin_setting_block_based_member_front() {
+	$disable         = get_option( 'reception_disable_block_based_member_front' );
+	$default_page_id = bp_get_option( '_reception_default_template_id', 0 );
+	?>
+	<input id="reception_disable_block_based_member_front" name="reception_disable_block_based_member_front" type="checkbox" value="1" <?php checked( ! $disable ); ?> />
+	<label for="reception_disable_block_based_member_front">
+		<?php esc_html_e( 'Utiliser le gabarit basé sur des blocs WordPress.', 'reception' ); ?>
+	</label>
+	<?php if ( $default_page_id && ! $disable ) : ?>
+		<p class="description">
+			<?php
+			printf(
+				/* translators: %s is the edit link to the Block based member's front template page */
+				esc_html__( 'Vous pouvez %s ce gabarit à tout moment.', 'reception' ),
+				sprintf(
+					'<a href="%1$s">%2$s</a>',
+					esc_url( get_edit_post_link( $default_page_id ) ),
+					esc_html__( 'modifier', 'reception' )
+				)
+			)
+			?>
+		</p>
+	<?php endif; ?>
+	<?php
+}
+
+/**
+ * Outputs the form elements to set the site owner preference about the customization of the Block based member front.
+ *
+ * @since 1.0.0
+ */
+function reception_admin_setting_members_block_based_front_customs() {
+	$enable = get_option( 'reception_allow_members_block_based_front_customs' ) && ! get_option( 'reception_disable_block_based_member_front' );
+	?>
+	<input id="reception_allow_members_block_based_front_customs" name="reception_allow_members_block_based_front_customs" type="checkbox" value="1" <?php checked( $enable ) . ' ' . disabled( true ); ?> />
+	<label for="reception_allow_members_block_based_front_customs">
+		<?php esc_html_e( 'Autoriser les membres à personnaliser le gabarit basé sur des blocs WordPress.', 'reception' ); ?>
+	</label>
+	<p class="description"><?php esc_html_e( 'Cette possibilité n’est pas disponible pour le moment.', 'reception' ); ?></p>
+	<?php
+}
+
+/**
+ * Registers specific Réception settings into the Members section of the BuddyPress options page.
+ *
+ * @since 1.0.0
+ */
+function reception_admin_register_settings() {
+	$disabled_block_based_member_front = get_option( 'reception_disable_block_based_member_front' );
+
+	register_setting(
+		'buddypress',
+		'reception_disable_block_based_member_front',
+		array(
+			'type'              => 'boolean',
+			'description'       => __( 'Indique s’il faut maintenir actif (`true`) ou désactiver (`false`) la page d’accueil des membres basée sur des blocs WordPress.', 'reception' ),
+			'sanitize_callback' => 'reception_admin_sanitize_block_based_member_front',
+			'show_in_rest'      => false,
+			'default'           => false,
+		)
+	);
+
+	add_settings_field(
+		'reception_disable_block_based_member_front',
+		__( 'Page d’accueil des membres', 'reception' ),
+		'reception_admin_setting_block_based_member_front',
+		'buddypress',
+		'bp_members'
+	);
+
+	if ( ! $disabled_block_based_member_front ) {
+		register_setting(
+			'buddypress',
+			'reception_allow_members_block_based_front_customs',
+			array(
+				'type'              => 'boolean',
+				'description'       => __( 'Indique s’il faut autoriser les membres à personnaliser le gabarit de leur page d’accueil.', 'reception' ),
+				'sanitize_callback' => 'rest_sanitize_boolean',
+				'show_in_rest'      => false,
+				'default'           => false,
+			)
+		);
+
+		add_settings_field(
+			'reception_allow_members_block_based_front_customs',
+			__( 'Personnalisation de la page d’accueil des membres', 'reception' ),
+			'reception_admin_setting_members_block_based_front_customs',
+			'buddypress',
+			'bp_members'
+		);
+	}
+}
+add_action( 'bp_register_admin_settings', 'reception_admin_register_settings', 11 );
